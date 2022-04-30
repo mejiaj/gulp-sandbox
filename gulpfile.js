d1a55fa16ffa710a25894757c2594e97c3f9c8a0
James Mejia <s.jamesmejia@gmail.com>
2022-04-30 15:06:14 -0500

    Browsersync reload w/ 3rd party compile

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,35 @@
-const { src, dest, task } = require("gulp");
+const { src, watch, series, parallel } = require("gulp")
+const uswds = require("@uswds/compile");
+const browserSync = require("browser-sync").create()
+
+uswds.settings.version = 3;
+uswds.paths.dist.theme = './sass';
+
+function serve() {
+  browserSync.init({
+    server: "."
+  })
+
+  // watch(uswds.paths.dist.theme, myWatchTask)
+  watch(uswds.paths.dist.theme, series(uswds.compileSass, cssInject))
+}
+
+function cssInject() {
+  return src(uswds.paths.dist.theme)
+    .pipe(browserSync.stream())
+}
+
+
+// async function myWatchTask(done) {
+//   const uswdsCompile = await uswds.compileSass
+//   uswdsCompile()
+//   cssInject()
+//   // This works, but triggers full refresh
+//   // browserSync.reload()
+//   done()
+// }
+
+
+// exports.myWatchTask = series(uswds.compileSass, browserSync.reload)
+exports.compile = uswds.compileSass
+exports.default = serve
