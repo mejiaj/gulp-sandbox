dbc0908ed11ab381edda687c8be12c79669dee94
James Mejia <james.mejia@gsa.gov>
2022-07-18 11:09:00 -0500

    Add server task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,23 @@
 const gulp = require("gulp");
 const uswds = require("@uswds/compile");
+const browserSync = require("browser-sync").create();
 
 uswds.settings.version = 3;
 
+function serve() {
+  browserSync.init({
+    server: "."
+  })
+
+  // watch(uswds.paths.dist.theme, myWatchTask)
+  gulp.watch(uswds.paths.dist.theme, gulp.series(uswds.compileSass, cssInject))
+}
+
+function cssInject() {
+  return src(uswds.paths.dist.theme)
+    .pipe(browserSync.stream())
+}
+
+exports.serve = serve;
 exports.initUSWDS = uswds.init;
 exports.compileSass = uswds.compileSass;
